0e858baacf7335c2e57af0684bf93510506c757f
GiovanniSchiavo <129737128+GiovanniSchiavo@users.noreply.github.com>
2024-09-17 20:35:20 +0200

    Corrected vercel url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   tagline: 'From students to students',
 
   // Set the production url of your site here
-  url: 'https://open-physica.vercel.app/',
+  url: 'https://openphysica.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
